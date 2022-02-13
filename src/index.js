fd568947489da57f22cb7dd6b47a71e492a4802c
KhrystynaHirna <86721411+KhrystynaHirna@users.noreply.github.com>
2022-02-14 00:33:14 +0100

    add fetch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import './css/styles.css';
+import debounce from 'lodash/debounce';
+import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+const input = document.querySelector('input#search-box');
 const DEBOUNCE_DELAY = 300;
+
+input.addEventListener('input', debounce(onInputSearch, DEBOUNCE_DELAY));
+
+fetchCountries();
+
+function fetchCountries(name) {
+    fetch('https://restcountries.com/v2/name/{name}')
+        .then(response => {
+            return response.json();
+        })
+        .then(country => {
+            console.log(country);
+        })
+        .catch(error => {
+            console.log(error);
+        })        
+}
+
+function onInputSearch() {
+
+}
